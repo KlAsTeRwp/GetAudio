c88a4fa077c9999d1c57d83e690699711954ce7c
Nikita <stalkera.123@gmail.com>
2017-05-19 01:27:40 +0300

    Added 3 catalogs in 'Domain'. Moove Music.cs into the 'Entities'

---
+++ Domain/Entities/Music.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    [Table("Musics")]
+    public class Music
+    {
+        [Key]
+        public int MusicID { get; set; }
+        public string Name { get; set; }
+        public byte[] File { get; set; }
+    }
+}
